4c5cab5a3cd2e6d78064200577ca60f2a7e969f0
Anil Gupta <anil.gupta@loginradius.com>
2019-12-09 17:18:10 +0530

    update

---
+++ SourceCode/Demo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Demo.Models;
+using IOSdk;
+using IOSdk.RequestModels;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var registerPayload = new UserRegisterationModel();
+            registerPayload.Email = "a@mailazy.com";
+            registerPayload.Password = "123456";
+            var result = new API().Register(registerPayload);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
