47bf4c5233f39423ed662a3a567f175409d7790d
hemant404 <56862469+hemant404@users.noreply.github.com>
2019-12-09 16:28:59 +0530

    Request and Response models folder added.

---
+++ SourceCode/IOSdk/RequestModels/UserRegistrationModelRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOSdk.RequestModels
+{
+    public class UserRegisterationModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Prefix { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Suffix { get; set; }
+        public string Gender { get; set; }
+    }
+}
